1269a63d0fde650ecc5e1175a8e33283de8ed52e
qzylalala <304228244@qq.com>
2021-10-25 11:34:49 +0800

    add comment

--- fronted/src/api/index.js
+++ fronted/src/api/index.js
@@ -3,19 +3,23 @@ let socket = new WebSocket("ws://localhost:8080/ws")
 let connect = cb => {
     console.log("connecting");
 
+    // 指定连接成功后的回调函数
     socket.onopen = () => {
         console.log("Successfully Connected");
     };
 
+    // 指定收到服务器数据后的回调函数
     socket.onmessage = msg => {
         console.log(msg);
         cb(msg);
     };
 
+    // 指定连接关闭后的回调函数
     socket.onclose = event => {
         console.log("Socket Closed Connection: ", event);
     };
 
+    // 指定报错时的回调函数
     socket.onerror = error => {
         console.log("Socket Error: ", error);
     };
